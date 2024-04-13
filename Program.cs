dac9ca2274d442bfb645893505d4716c431f7b4a
HugoSFontes <hugo.henriquefontes@hotmail.com>
2024-04-13 00:28:03 -0300

    push
    criação de herança nas classes para polimorfismo e instanciando para testes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System.Threading.Channels;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+ Console.WriteLine("Aparelho Nokia:");
+ Nokia nokia = new Nokia("123321123", "nokia 720", "1111-01", 8);
+ nokia.Ligar();
+ nokia.InstalarAplicativo("Arknights");
+
+ Console.WriteLine("");
+ 
+ Console.WriteLine("Aparelho Iphone:");
+ Iphone iphone = new Iphone("00123213", "Iphone X", "12223-02", 16);
+iphone.ReceberLigacao();
+ iphone.InstalarAplicativo("Blue Archive");
